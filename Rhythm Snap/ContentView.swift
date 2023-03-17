4394b735e2dd70de144979c1260da868c90a91c5
Carlos Mbendera <carlosmbendera@gmail.com>
2023-03-17 18:25:44 -0500

    Initial Commit

---
+++ Rhythm Snap/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Rhythm Snap
+//
+//  Created by Carlos Mbendera on 2023-03-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Rhythm Snap/Rhythm_SnapApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rhythm_SnapApp.swift
+//  Rhythm Snap
+//
+//  Created by Carlos Mbendera on 2023-03-17.
+//
+
+import SwiftUI
+
+@main
+struct Rhythm_SnapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
